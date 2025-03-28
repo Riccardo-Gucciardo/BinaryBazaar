7a5f06b2dfbd4e8a340a11811bd363e4628a2b88
giuseppezaccato <giuseppezaccato@gmail.com>
2025-03-28 18:10:17 +0100

    ADD: cors

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
-const port = process.env.SERVER_PORT||3000;
+const port = process.env.SERVER_PORT || 3000;
+const endpoint = process.env.FRONTEND_PORT
 
 import productRouter from './routers/productRouter.js'
 
+//cors
+app.use(cors({
+    origin: endpoint
+}))
+
+//body parse
 app.use(express.json())
-app.use('/products',productRouter)
+
+//router
+app.use('/products', productRouter)
 
 
 
@@ -13,4 +24,3 @@ app.use('/products',productRouter)
 app.listen(port, () => {
     console.log(`server in funzione sulla porta: ${port}`)
 })
- 
