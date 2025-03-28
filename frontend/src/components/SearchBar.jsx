d9351f298b8838b8d514605c2a78f2101bb958a7
IgorGoro91 <igorysc@gmail.com>
2025-03-28 17:34:25 +0100

    add: Frontend

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,56 @@
+
+import { useState } from "react";
+
+const productsList = [
+  "Mela",
+  "Banana",
+  "Arancia",
+  "Computer",
+  "Telefono",
+  "Tastiera",
+  "Mouse",
+  "Auricolari",
+];
+
+function SearchBar() {
+  const [search, setSearch] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const handleSearch = () => {
+    const filtered = productsList.filter((product) =>
+      product.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
+
+  return (
+    <div className="search-block">
+      <h1 className="search-block__title">Scegli il tuo prodotto</h1>
+      <div className="search-block__body">
+        <div className="search-block__wrap-input">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Tuo prodotto"
+            className="search-block__input"
+          />
+          <div className="btn-box">
+            <button onClick={handleSearch}>Cerca</button>
+          </div>
+        </div>
+
+        <ul className="search-block__products">
+          {filteredProducts.map((product, index) => (
+            <li key={index}>{product}</li>
+          ))}
+          {filteredProducts.length === 0 && search !== "" && (
+            <li>Nessun prodotto trovato</li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
