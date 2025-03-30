e9d78a464252ca59201ae05a67c1c20d9d384703
giuseppezaccato <giuseppezaccato@gmail.com>
2025-03-30 19:52:01 +0200

    ADD: multerTry

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,27 +2,28 @@ import connection from '../data/db.js'
 
 
 
-function index(req,res){
+function index(req, res) {
 
 
     const sql = 'SELECT * FROM products'
 
-    connection.query( sql, (err, results) => {
-        if(err) return res.status(500).json({
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({
             error: 'error'
         })
 
-        // const products = results.map(product =>{
-        //     return{
-        //         ...product,
-        //         image: req.imagePath + product.image
-        //     }
-                
-        // })
-        res.json(results)
+        // res.json(results); //* AGGIORNAMENTO CON USO MIDDLEWARE
+        const products = results.map(p => {
+            return {
+                ...p,
+                image_url: `${req.imagePath}${p.slug}.webp`
+            }
+
+        });
+        res.json(products)
     })
 
-    
+
 
 }
 
