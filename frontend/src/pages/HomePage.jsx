24dffa38b22b4d1ca55ba365a1aef0dcddfbd35e
giuseppezaccato <giuseppezaccato@gmail.com>
2025-03-31 14:45:43 +0200

    ADD: container

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,27 +1,27 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
 // import ProductsList from "./ProductsList"
 import Card from "../components/Card"
 import Hero from "../components/hero"
-import { useEffect,useState } from "react"
-import axios from 'axios'
 
 export default function HomePage() {
 
-        const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([])
 
-    const fetchProducts =()=> {
+    const fetchProducts = () => {
         axios.get('http://localhost:3000/products')
-        .then(res => setProducts (res.data) )
-        .catch(err => console.error(err))
+            .then(res => setProducts(res.data))
+            .catch(err => console.error(err))
     }
 
-    useEffect(fetchProducts,[])
+    useEffect(fetchProducts, [])
 
     const renderPoducts = () => {
-        return(
-            products.map( (product) => {
-                return(
-                    <div className="col" key={product.id}>
-                        <Card product={product}/>
+        return (
+            products.map((product) => {
+                return (
+                    <div key={product.id}>
+                        <Card product={product} />
                     </div>
                 )
             })
@@ -29,17 +29,16 @@ export default function HomePage() {
     }
 
 
-
     return (
         <>
-        <Hero/>
-        <h1>i nostri prodotti</h1>
-       <div className="row row-cols-md-3 row-cols-lg-4">
-        {/* <Card/> */}
-        {renderPoducts()}
-        </div> 
-        
-        
+            <Hero />
+            <h1>i nostri prodotti</h1>
+            <div className="container mx-auto row row-cols-md-3 row-cols-lg-4 g-1">
+                {/* <Card/> */}
+                {renderPoducts()}
+            </div>
+
+
         </>
     )
 }
