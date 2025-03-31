3c98e05e49758de8ceafe054bd05c4c76e6a6909
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-03-31 10:44:56 +0200

    hero

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 // import ProductsList from "./ProductsList"
 import Card from "../components/Card"
+import Hero from "../components/hero"
 
 export default function HomePage() {
     return (
         <>
+        <Hero/>
         <h1>gdsag</h1>
         
         <Card/>
