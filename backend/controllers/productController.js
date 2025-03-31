7226eb995b0f25d3b1260ff2487766e267510178
manuelpezzi <pezzimanuel@gmail.com>
2025-03-31 11:39:36 +0200

    add:laptop id call

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,33 +24,63 @@ function index(req, res) {
     })
 }
 
+// function showLaptop(req, res) {
+//     const { slug, id } = req.params
+//     const laptopSql = 'SELECT * FROM laptop_details WHERE product_id = ?';
+
+//     const sql = "SELECT * FROM products WHERE slug=?"
+//     connection.query(sql, [slug], (err, results) => {
+//         if (err) return res.status(500).json({
+//             error: 'error'
+//         })
+//         results.length == 0 && res.status(404).json({ error: 'Prodotto Non Trovato' });
+
+//         const laptop = results[0]
+//         connection.query(laptopSql, [id], (err, laptopResults) => {
+//             if (err) return res.status(500).json({
+//                 error: 'error'
+//             })
+
+
+//             // Rimuovi l'id dai dettagli del laptop
+//             laptop.laptop_details = laptopResults.map(detail => {
+//                 const { id, ...rest } = detail; // Escludi l'id
+//                 return rest;
+//             });
+
+
+//             laptop.laptop_details = laptopResults;
+//             res.json({
+//                 ...laptop,
+//                 image_url: `${req.imagePath}${laptop.slug}.webp`
+//             })
+//         })
+
+
+
+//     })
+// }
 function showLaptop(req, res) {
-    const { slug, id } = req.params
-    const laptopSql = 'SELECT * FROM laptop_details WHERE product_id = ?';
+    const { slug } = req.params;
+    const sql = "SELECT * FROM products WHERE slug=?";
 
-    const sql = "SELECT * FROM products WHERE slug=?"
     connection.query(sql, [slug], (err, results) => {
-        if (err) return res.status(500).json({
-            error: 'error'
-        })
-        results.length == 0 && res.status(404).json({ error: 'Prodotto Non Trovato' });
+        if (err) return res.status(500).json({ error: 'error' });
+        if (results.length === 0) return res.status(404).json({ error: 'Prodotto Non Trovato' });
 
-        const laptop = results[0]
-        connection.query(laptopSql, [id], (err, laptopResults) => {
-            if (err) return res.status(500).json({
-                error: 'error'
-            })
+        const laptop = results[0];
+        const laptopSql = 'SELECT * FROM laptop_details WHERE product_id = ?'; //Ora si usa il product_id dal result della prima query.
+
+        connection.query(laptopSql, [laptop.product_id], (err, laptopResults) => {
+            if (err) return res.status(500).json({ error: 'error' });
             laptop.laptop_details = laptopResults;
 
             res.json({
                 ...laptop,
                 image_url: `${req.imagePath}${laptop.slug}.webp`
-            })
-        })
-
-
-
-    })
+            });
+        });
+    });
 }
 
 
