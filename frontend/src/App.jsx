007cc6a746710c5a975d6caad11f7fe76c4febcb
giuseppezaccato <giuseppezaccato@gmail.com>
2025-04-02 16:56:48 +0200

    ADD: CheckOut page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import DefaultLayout from "./layouts/DefaultLayout";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import CheckOut from './pages/CheckOut'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route Component={DefaultLayout}>
             <Route path="/" Component={HomePage} />
             <Route path="/:slug/" Component={ProductPage} />
+            <Route path="/checkout" Component={CheckOut} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ frontend/src/pages/CheckOut.jsx
@@ -0,0 +1,10 @@
+export default function CheckOut() {
+    return (
+
+
+        <h1>PAGINA CHECKOUT</h1>
+    )
+
+
+
+}
