aedcf57788abb98d8d993d693629637241277b4c
IgorGoro91 <igorysc@gmail.com>
2025-04-03 17:58:46 +0200

    add:Burger

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,31 +1,45 @@
-import { NavLink } from "react-router-dom"
-import SearchBar from "./SearchBar"
+import { NavLink } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 export default function NavBar() {
     return (
-
         <header className="header">
-        <NavLink to="/"> <img src="/Logo.png" alt="" className="img-logo" /> </NavLink>
-    
-        <div className="search-container">
-            <SearchBar />
-        </div>
-    
-        <ul className="nav">
-            <li className="nav-item">
-                <NavLink className="nav-link active" to="/" aria-current="page">
-                    HomePage
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/:slug">
-                    I Nostri Prodotti
-                </NavLink>
-            </li>
-        </ul>
-    </header>
+            <NavLink to="/"> 
+                <img src="/Logo.png" alt="" className="img-logo" />
+            </NavLink>
 
+            {/* SearchBar Desktop */}
+            <div className="desktop-search">
+                <SearchBar />
+            </div>
 
+            {/* Menu Hamburger */}
+            <input type="checkbox" className="menu-checkbox" id="menu-toggle" />
+            <label className="menu-toggle" htmlFor="menu-toggle">
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </label>
 
-    )
+            {/* Menu Items */}
+            <div className="nav-menu">
+                {/* SearchBar Mobile */}
+                <div className="mobile-search">
+                    <SearchBar />
+                </div>
+                <ul className="nav">
+                    <li className="nav-item">
+                        <NavLink className="nav-link active" to="/" aria-current="page">
+                            HomePage
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/:slug">
+                            I Nostri Prodotti
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    );
 }
